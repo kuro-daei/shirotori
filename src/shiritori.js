eff2e56ca6e8b2a573aa53898f4c22a0f29c5956
kuro-daei <eiji@kuroda.me>
2020-04-19 09:42:35 +0900

    upd

--- src/shiritori.js
+++ src/shiritori.js
@@ -1,6 +1,4 @@
 export default function shiritori(inputWord, lastChar, wordLen) {
-  console.log(`入力 : ${inputWord}`);
-  console.log(`最後の文字 : ${lastChar}`);
-  console.log(`文字数 : ${wordLen}`);
+  console.log(`inputWord=${inputWord},lastChar=${lastChar},wordLen=${wordLen}`);
   return true;
 }
